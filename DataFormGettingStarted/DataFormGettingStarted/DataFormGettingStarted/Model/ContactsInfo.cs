207cd5208dde78c716799a43b23addb39f1d8ad8
ROOT\swathi.gopal <swathi.gopal@syncfusion.com>
2019-01-02 12:32:50 +0530

    Included Getting Started sample and readme content file.

---
+++ DataFormGettingStarted/DataFormGettingStarted/DataFormGettingStarted/Model/ContactsInfo.cs
@@ -0,0 +1,97 @@
+﻿using Syncfusion.XForms.DataForm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFormGettingStarted
+{    
+    public class ContactsInfo
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private string contactNo;
+        private string email;
+        private string address;
+        private DateTime? birthDate;
+        private string groupName;
+
+        public ContactsInfo()
+        {
+
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public string MiddleName
+        {
+            get { return this.middleName; }
+            set
+            {
+                this.middleName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
+        public string ContactNumber
+        {
+            get { return contactNo; }
+            set
+            {
+                this.contactNo = value;
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+            }
+        }
+
+        public DateTime? BirthDate
+        {
+            get { return birthDate; }
+            set
+            {
+                birthDate = value;
+            }
+        }
+
+        public string GroupName
+        {
+            get { return groupName; }
+            set
+            {
+                groupName = value;
+            }
+        }
+    }
+}
---
+++ DataFormGettingStarted/DataFormGettingStarted/DataFormGettingStarted/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace DataFormGettingStarted
+{
+    public class ViewModel
+    {
+        private ContactsInfo contactsInfo;
+        public ContactsInfo ContactsInfo
+        {
+            get { return this.contactsInfo; }
+            set { this.contactsInfo = value; }
+        }
+        public ViewModel()
+        {
+            this.contactsInfo = new ContactsInfo();
+        }
+    }
+}
